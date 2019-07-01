805b9d3792bdda39a4cd2dd45bfd8d4f59e80827
qk <quankang@kylinos.cn>
2019-07-01 09:02:14 +0800

    Resolve taskbar copyright time display error

--- ukui-panel/panel-context-menu.c
+++ ukui-panel/panel-context-menu.c
@@ -142,7 +142,7 @@ panel_context_menu_show_about_dialog (GtkWidget *menuitem)
 		      "program-name",  _("The UKUI Panel"),
 		      "version", VERSION,
 		      "copyright", "Copyright \xc2\xa9 1997-2003 Free Software Foundation, Inc.\n"
-		                   "Copyright \xc2\xa9 2011-2017 UKUI Developers.",
+		                   "Copyright \xc2\xa9 2011-2019 UKUI Developers.",
 		      "comments", _("This program is responsible for launching other "
 				    "applications and provides useful utilities."),
 		      "authors", authors,
