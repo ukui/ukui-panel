6b0e635beb9ab9d482b7e3634062a27c3aa84e1b
hepuyao <2410474020@qq.com>
2020-01-17 17:04:57 +0800

    Remove old files(UKUI2.0)

--- ukui-panel/libukui-panel-applet-private/panel-applet-mini.c
+++
@@ -1,5 +0,0 @@
-/* Symbols needed for libukui-panel-applet-private-mini, which is used by the test
- * program */
-#include <glib.h>
-gboolean ukui_panel_applet_frame_dbus_load (const gchar *iid, gpointer frame_act);
-gboolean ukui_panel_applet_frame_dbus_load (const gchar *iid, gpointer frame_act) { return FALSE; }
