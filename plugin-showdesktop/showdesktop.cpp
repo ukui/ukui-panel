9862437153d4f0ab5151cd7be114f760cdcafe0e
hepuyao <2410474020@qq.com>
2020-02-04 20:37:13 +0800

    iconSize

--- plugin-showdesktop/showdesktop.cpp
+++ plugin-showdesktop/showdesktop.cpp
@@ -63,7 +63,7 @@ void ShowDesktop::realign()
         mButton.setStyleSheet(
                     //正常状态样式
                     "QToolButton{"
-                    /*"background-color:rgba(100,225,100,80%);"//背景色（也可以设置图片）*/
+                    "background-color:rgba(190,216,239,10%);" //背景色（也可以设置图片）
                     "qproperty-iconSize:40px 40px;"
                     "border-style:outset;"                  //边框样式（inset/outset）
                     "border-width:0px;"                     //边框宽度像素
