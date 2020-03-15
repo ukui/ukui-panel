b4ce3e0820446a57d5e85291c66e434f9a04ed1d
hepuyao <2410474020@qq.com>
2020-03-15 19:05:14 +0800

    modify copyright

--- panel/common/ukuibacklight/virtual_backend.h
+++ panel/common/ukuibacklight/virtual_backend.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2016  P.L. Lucas
- * 
+ * Copyright (c) 2019 Tianjin KYLIN Information Technology Co., Ltd. * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
