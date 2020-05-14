f1a27897dd1aa89b54ea0b7fd6805a7bbd97bb05
hepuyao <2410474020@qq.com>
2020-05-14 21:04:42 +0800

    highlight tray icon

--- panel/resources/ukui-panel_bo.ts
+++ panel/resources/ukui-panel_bo.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh_CN">
+<TS version="2.1" language="bo">
 <context>
     <name>AddPluginDialog</name>
     <message>
