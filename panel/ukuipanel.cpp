84213960fc3a58ce70b6a3fc4dfc2cce05ffcf16
hepuyao <2410474020@qq.com>
2020-05-21 19:22:24 +0800

    change panel window Flags

--- panel/ukuipanel.cpp
+++ panel/ukuipanel.cpp
@@ -177,7 +177,7 @@ UKUIPanel::UKUIPanel(const QString &configGroup, UKUi::Settings *settings, QWidg
     // it's not possible to use the panel with keyboards. We need to find a better solution later.
 
     //部分组建在点击任务栏空白位置的时候，无法收回窗口，想要正常收回窗口，需要取消下面的窗口属性或者其他应用监听点击taskbar的点击信号
-//    flags |= Qt::WindowDoesNotAcceptFocus;
+    flags |= Qt::WindowDoesNotAcceptFocus;
 
     setWindowFlags(flags);
     //Adds _NET_WM_WINDOW_TYPE_DOCK to the window's _NET_WM_WINDOW_TYPE X11 window property. See https://standards.freedesktop.org/wm-spec/ for more details.
