d7fe120ad53f0e6ed4ae09d57b73af946d18f105
minglequn <verymerry@163.com>
2020-05-28 13:54:17 +0800

    commit

--- plugin-taskbar/ukuitaskbar.cpp
+++ plugin-taskbar/ukuitaskbar.cpp
@@ -285,6 +285,7 @@ void UKUITaskBar::addWindow(WId window)
 {
     // If grouping disabled group behaves like regular button
     const QString group_id = mGroupingEnabled ? KWindowInfo(window, 0, NET::WM2WindowClass).windowClassClass() : QString("%1").arg(window);
+    qDebug()<<"UKUITaskBar::addWindow(WId window)"<<group_id;
 #if QT_VERSION < 0x050600
     if(!group_id.compare("penoy-qt-desktop"))
     {
