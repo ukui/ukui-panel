fb65b2c6fecf2af4f6a7e592b7492828a278b871
minglequn <54056833+minglequn@users.noreply.github.com>
2020-06-18 14:52:48 +0800

    v10 minize and maximize not take effect (#369)

--- plugin-taskbar/ukuitaskwidget.cpp
+++ plugin-taskbar/ukuitaskwidget.cpp
@@ -591,7 +591,9 @@ bool UKUITaskWidget::isMinimized() const
 {
     //    return KWindowInfo(mWindow,NET::WMState | NET::XAWMState).isMinimized();
 #if (QT_VERSION >= QT_VERSION_CHECK(5,7,0))
-    return NET::Focused == (KWindowInfo(mWindow,NET::WMState).state()&NET::Focused);    
+    return NET::Focused == (KWindowInfo(mWindow,NET::WMState).state()&NET::Focused);
+#else
+    return isApplicationActive();
 #endif
 }
 
@@ -600,6 +602,8 @@ bool UKUITaskWidget::isFocusState() const
     qDebug()<<"KWindowInfo(mWindow,NET::WMState).state():"<<KWindowInfo(mWindow,NET::WMState).state();
 #if (QT_VERSION >= QT_VERSION_CHECK(5,7,0))
     return NET::Focused == (KWindowInfo(mWindow,NET::WMState).state()&NET::Focused);
+#else
+    return isApplicationActive();
 #endif
 }
 
