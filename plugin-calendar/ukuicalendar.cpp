cab41c8ef7697fd7f5735bf7823ef0d0b3a5d536
JK-Lix <776579471@qq.com>
2020-07-10 10:15:54 +0800

    About the font which showed in date of panel

--- plugin-calendar/ukuicalendar.cpp
+++ plugin-calendar/ukuicalendar.cpp
@@ -169,27 +169,6 @@ IndicatorCalendar::IndicatorCalendar(const IUKUIPanelPluginStartupInfo &startupI
     }
 
     setTimeShowStyle();
-    mContent->setStyleSheet(
-                //正常状态样式
-                "QLabel{"
-                /*"background-color:rgba(100,225,100,80%);"//背景色（也可以设置图片）*/
-                "border-width:0px;"                        //边框宽度像素
-                "border-radius:6px;"                       //边框圆角半径像素
-                "font: SourceHanSansCN-Medium  12px;"      //字体，字体大小
-                "color:rgba(255,255,255,100%);"            //字体颜色
-                "padding:0px;"                             //填衬
-                "textalignment:aligncenter"                //文本居中
-                "}"
-                //鼠标悬停样式
-                "QLabel:hover{"
-                "background-color:rgba(190,216,239,20%);"
-                "}"
-                //鼠标按下样式
-                "QLabel:pressed{"
-                "background-color:rgba(190,216,239,12%);"
-                "}"
-
-                );
     mContent->setWordWrap(true);
     setToolTip();
 }
@@ -328,6 +307,26 @@ void IndicatorCalendar::updateTimeText()
     mContent->setText(str);
     if (old_size != mContent->sizeHint())
         mRotatedWidget->adjustContentSize();
+    mContent->setStyleSheet(
+                //正常状态样式
+                "QLabel{"
+                "border-width:  0px;"                     //边框宽度像素
+                "border-radius: 6px;"                       //边框圆角半径像素
+                "font-size:     14px;"                      //字体，字体大小
+                "color:         rgba(255,255,255,100%);"    //字体颜色
+                "padding:       0px;"                       //填衬
+                "text-align:center;"                        //文本居中
+                "}"
+                //鼠标悬停样式
+                "QLabel:hover{"
+                "background-color:rgba(190,216,239,20%);"
+                "border-radius:6px;"                       //边框圆角半径像素
+                "}"
+                //鼠标按下样式
+                "QLabel:pressed{"
+                "background-color:rgba(190,216,239,12%);"
+                "}"
+                );
     mRotatedWidget->update();
     updatePopupContent();
     mbIsNeedUpdate = false;
@@ -832,11 +831,11 @@ void IndicatorCalendar::setTimeShowStyle()
 {
     if(panel()->isHorizontal())
     {
-        mContent->setFixedSize(CALENDAR_WIDTH,CALENDAR_HEIGHT);
+        mContent->setFixedSize(CALENDAR_WIDTH,panel()->panelSize() - 5); //CALENDAR_HEIGHT);//panel()->panelSize() - 5);
     }
     else
     {
-        mContent->setFixedSize(CALENDAR_HEIGHT,CALENDAR_WIDTH);
+        mContent->setFixedSize(panel()->panelSize() - 5,CALENDAR_WIDTH);
     }
     mbIsNeedUpdate = true;
     timeout();
