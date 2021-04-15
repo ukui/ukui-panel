0ebca29f35d927af36f0dfe995fa67d087cfe20f
hepuyao <2410474020@qq.com>
2021-04-15 15:01:06 +0800

    解决预览窗口状态切换的时候预览异常的bug

--- plugin-taskbar-wayland/ukuitaskgroup.cpp
+++ plugin-taskbar-wayland/ukuitaskgroup.cpp
@@ -1229,15 +1229,26 @@ bool UKUITaskGroup::isSetMaxWindow()
 void UKUITaskGroup::showPreview()
 {
     int n = 7;
-    if (plugin()->panel()->isHorizontal()) n = 10;
-    if(mVisibleHash.size() <= n)
-    {
-        showAllWindowByThumbnail();
-    }
-    else
-    {
-        showAllWindowByList();
-    }
+        if (plugin()->panel()->isHorizontal()) n = 10;
+        if(mVisibleHash.size() <= n)
+        {
+    //        showAllWindowByThumbnail();
+            if (mPopup->isHidden()) {
+                qDebug()<<"";
+                showAllWindowByThumbnail();
+
+            } else {
+                imagicFlag = false;
+                mPopup->hide(true);
+                showAllWindowByThumbnail();//
+                imagicFlag = true;
+            }
+        }
+        else
+        {
+            showAllWindowByList();
+        }
+
 }
 
 void UKUITaskGroup::adjustPopWindowSize(int winWidth, int winHeight)
@@ -1670,10 +1681,12 @@ void UKUITaskGroup::showAllWindowByThumbnail()
     {
 //        mPopup-
     }
-    else
     {
-        mPopup->show();
+//        mPopup->show();
+        if(imagicFlag)
+             mPopup->show();
     }
+
 
 //   emit popupShown(this);
 }
