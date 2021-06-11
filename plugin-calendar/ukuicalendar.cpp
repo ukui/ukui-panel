7401ea9cbb20e7f8f22eac83a705327a252a4e08
liyanda <2772454664@qq.com>
2021-06-11 14:08:32 +0800

    调整日历与任务栏间距

--- plugin-calendar/ukuicalendar.cpp
+++ plugin-calendar/ukuicalendar.cpp
@@ -568,20 +568,16 @@ void CalendarActiveLabel::mousePressEvent(QMouseEvent *event)
             int totalWidth = qApp->primaryScreen()->size().width() + qApp->primaryScreen()->geometry().x();
             switch (mPlugin->panel()->position()) {
             case IUKUIPanel::PositionBottom:
-                w->setGeometry(totalWidth-mViewWidht-4,totalHeight-mPlugin->panel()->panelSize()-mViewHeight-4,mViewWidht,mViewHeight);
-                qDebug()<<totalWidth-mViewWidht-4;
-                qDebug()<<totalHeight-mPlugin->panel()->panelSize()-mViewHeight-4;
-                qDebug()<<mViewWidht;
-                qDebug()<<mViewHeight;
+                w->setGeometry(totalWidth-mViewWidht-8,totalHeight-mPlugin->panel()->panelSize()-mViewHeight-8,mViewWidht,mViewHeight);
                 break;
             case IUKUIPanel::PositionTop:
-                w->setGeometry(totalWidth-mViewWidht-4,qApp->primaryScreen()->geometry().y()+mPlugin->panel()->panelSize()+4,mViewWidht,mViewHeight);
+                w->setGeometry(totalWidth-mViewWidht-8,qApp->primaryScreen()->geometry().y()+mPlugin->panel()->panelSize()+8,mViewWidht,mViewHeight);
                 break;
             case IUKUIPanel::PositionLeft:
-                w->setGeometry(qApp->primaryScreen()->geometry().x()+mPlugin->panel()->panelSize()+4,totalHeight-mViewHeight-4,mViewWidht,mViewHeight);
+                w->setGeometry(qApp->primaryScreen()->geometry().x()+mPlugin->panel()->panelSize()+8,totalHeight-mViewHeight-8,mViewWidht,mViewHeight);
                 break;
             case IUKUIPanel::PositionRight:
-                w->setGeometry(totalWidth-mPlugin->panel()->panelSize()-mViewWidht-4,totalHeight-mViewHeight-4,mViewWidht,mViewHeight);
+                w->setGeometry(totalWidth-mPlugin->panel()->panelSize()-mViewWidht-8,totalHeight-mViewHeight-8,mViewWidht,mViewHeight);
                 break;
             default:
                 w->setGeometry(qApp->primaryScreen()->geometry().x()+mPlugin->panel()->panelSize()+4,totalHeight-mViewHeight,mViewWidht,mViewHeight);
