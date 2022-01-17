b5388141f3ebad62062cbae2267a304cc970a36f
buxiaoqing <buxiaoqing@kylinos.cn>
2022-01-17 15:58:14 +0800

    修改翻译

---
+++ plugin-taskbar/translation/taskbar_zh_CN.ts
@@ -0,0 +1,181 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>UKUITaskBar</name>
+    <message>
+        <location filename="../ukuitaskbar.cpp" line="1034"/>
+        <source>Drop Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukuitaskbar.cpp" line="1035"/>
+        <source>File/URL &apos;%1&apos; cannot be embedded into QuickLaunch for now</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UKUITaskButton</name>
+    <message>
+        <location filename="../ukuitaskbutton.cpp" line="583"/>
+        <source>Application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukuitaskbutton.cpp" line="617"/>
+        <source>To &amp;Desktop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukuitaskbutton.cpp" line="619"/>
+        <source>&amp;All Desktops</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukuitaskbutton.cpp" line="627"/>
+        <source>Desktop &amp;%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukuitaskbutton.cpp" line="634"/>
+        <source>&amp;To Current Desktop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukuitaskbutton.cpp" line="642"/>
+        <source>&amp;Move</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukuitaskbutton.cpp" line="645"/>
+        <source>Resi&amp;ze</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukuitaskbutton.cpp" line="652"/>
+        <source>Ma&amp;ximize</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukuitaskbutton.cpp" line="659"/>
+        <source>Maximize vertically</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukuitaskbutton.cpp" line="664"/>
+        <source>Maximize horizontally</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukuitaskbutton.cpp" line="670"/>
+        <source>&amp;Restore</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukuitaskbutton.cpp" line="674"/>
+        <source>Mi&amp;nimize</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukuitaskbutton.cpp" line="680"/>
+        <source>Roll down</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukuitaskbutton.cpp" line="686"/>
+        <source>Roll up</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukuitaskbutton.cpp" line="694"/>
+        <source>&amp;Layer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukuitaskbutton.cpp" line="696"/>
+        <source>Always on &amp;top</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukuitaskbutton.cpp" line="702"/>
+        <source>&amp;Normal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukuitaskbutton.cpp" line="708"/>
+        <source>Always on &amp;bottom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukuitaskbutton.cpp" line="716"/>
+        <source>&amp;Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukuitaskbutton.cpp" line="878"/>
+        <source>delete from quicklaunch</source>
+        <translation type="unfinished">从任务栏取消固定</translation>
+    </message>
+</context>
+<context>
+    <name>UKUITaskGroup</name>
+    <message>
+        <location filename="../ukuitaskgroup.cpp" line="255"/>
+        <source>Group</source>
+        <translation>组</translation>
+    </message>
+    <message>
+        <location filename="../ukuitaskgroup.cpp" line="262"/>
+        <source>delete from taskbar</source>
+        <translation>从任务栏取消固定</translation>
+    </message>
+    <message>
+        <location filename="../ukuitaskgroup.cpp" line="264"/>
+        <source>add to taskbar</source>
+        <translation>添加到任务栏</translation>
+    </message>
+    <message>
+        <location filename="../ukuitaskgroup.cpp" line="269"/>
+        <source>close</source>
+        <translation>关闭</translation>
+    </message>
+</context>
+<context>
+    <name>UKUITaskWidget</name>
+    <message>
+        <location filename="../ukuitaskwidget.cpp" line="448"/>
+        <source>Widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukuitaskwidget.cpp" line="451"/>
+        <source>close</source>
+        <translation>关闭</translation>
+    </message>
+    <message>
+        <location filename="../ukuitaskwidget.cpp" line="452"/>
+        <source>restore</source>
+        <translation>恢复</translation>
+    </message>
+    <message>
+        <location filename="../ukuitaskwidget.cpp" line="454"/>
+        <source>maximaze</source>
+        <translation>最大化</translation>
+    </message>
+    <message>
+        <location filename="../ukuitaskwidget.cpp" line="456"/>
+        <source>minimize</source>
+        <translation>最小化</translation>
+    </message>
+    <message>
+        <location filename="../ukuitaskwidget.cpp" line="457"/>
+        <source>above</source>
+        <translation>置顶</translation>
+    </message>
+    <message>
+        <location filename="../ukuitaskwidget.cpp" line="458"/>
+        <source>clear</source>
+        <translation>取消置顶</translation>
+    </message>
+</context>
+</TS>
