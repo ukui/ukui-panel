d024b0e414940f1f82a1864363efa4cdf33bc155
chenxuechao <chenxuechao@kylinos.cn>
2022-01-20 14:33:59 +0800

    删除多余文件

--- panel/translation/panel_zh_CN.ts
+++ panel/translation/panel_zh_CN.ts
@@ -4,52 +4,52 @@
 <context>
     <name>UKUIPanel</name>
     <message>
-        <location filename="../ukuipanel.cpp" line="929"/>
+        <location filename="../ukuipanel.cpp" line="925"/>
         <source>Small</source>
         <translation>小尺寸</translation>
     </message>
     <message>
-        <location filename="../ukuipanel.cpp" line="931"/>
+        <location filename="../ukuipanel.cpp" line="927"/>
         <source>Medium</source>
         <translation>中尺寸</translation>
     </message>
     <message>
-        <location filename="../ukuipanel.cpp" line="933"/>
+        <location filename="../ukuipanel.cpp" line="929"/>
         <source>Large</source>
         <translation>大尺寸</translation>
     </message>
     <message>
-        <location filename="../ukuipanel.cpp" line="938"/>
+        <location filename="../ukuipanel.cpp" line="934"/>
         <source>Adjustment Size</source>
         <translation>调整大小</translation>
     </message>
     <message>
-        <location filename="../ukuipanel.cpp" line="974"/>
+        <location filename="../ukuipanel.cpp" line="970"/>
         <source>Up</source>
         <translation>上</translation>
     </message>
     <message>
-        <location filename="../ukuipanel.cpp" line="976"/>
+        <location filename="../ukuipanel.cpp" line="972"/>
         <source>Bottom</source>
         <translation>下</translation>
     </message>
     <message>
-        <location filename="../ukuipanel.cpp" line="978"/>
+        <location filename="../ukuipanel.cpp" line="974"/>
         <source>Left</source>
         <translation>左</translation>
     </message>
     <message>
-        <location filename="../ukuipanel.cpp" line="980"/>
+        <location filename="../ukuipanel.cpp" line="976"/>
         <source>Right</source>
         <translation>右</translation>
     </message>
     <message>
-        <location filename="../ukuipanel.cpp" line="983"/>
+        <location filename="../ukuipanel.cpp" line="979"/>
         <source>Adjustment Position</source>
         <translation>调整位置</translation>
     </message>
     <message>
-        <location filename="../ukuipanel.cpp" line="1009"/>
+        <location filename="../ukuipanel.cpp" line="1005"/>
         <source>Hide Panel</source>
         <translation>隐藏任务栏</translation>
     </message>
@@ -58,43 +58,43 @@
         <translation type="vanished">设置任务栏</translation>
     </message>
     <message>
-        <location filename="../ukuipanel.cpp" line="1399"/>
+        <location filename="../ukuipanel.cpp" line="1395"/>
         <source>Show Taskview</source>
         <translation>显示任务视图按钮</translation>
     </message>
     <message>
-        <location filename="../ukuipanel.cpp" line="1416"/>
+        <location filename="../ukuipanel.cpp" line="1412"/>
         <source>Show Nightmode</source>
         <translation>显示夜间模式按钮</translation>
     </message>
     <message>
-        <location filename="../ukuipanel.cpp" line="1423"/>
+        <location filename="../ukuipanel.cpp" line="1419"/>
         <source>Show Desktop</source>
         <translation>显示桌面</translation>
     </message>
     <message>
-        <location filename="../ukuipanel.cpp" line="1431"/>
+        <location filename="../ukuipanel.cpp" line="1427"/>
         <source>Show System Monitor</source>
         <translation>系统监视器</translation>
     </message>
     <message>
-        <location filename="../ukuipanel.cpp" line="1456"/>
+        <location filename="../ukuipanel.cpp" line="1452"/>
         <source>Lock This Panel</source>
         <translation>锁定任务栏</translation>
     </message>
     <message>
-        <location filename="../ukuipanel.cpp" line="1471"/>
+        <location filename="../ukuipanel.cpp" line="1469"/>
         <source>About Kylin</source>
         <translation>关于麒麟</translation>
     </message>
     <message>
-        <location filename="../ukuipanel.cpp" line="1636"/>
+        <location filename="../ukuipanel.cpp" line="1625"/>
         <source>Remove Panel</source>
         <comment>Dialog Title</comment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ukuipanel.cpp" line="1637"/>
+        <location filename="../ukuipanel.cpp" line="1626"/>
         <source>Removing a panel can not be undone.
 Do you want to remove this panel?</source>
         <translation type="unfinished"></translation>
