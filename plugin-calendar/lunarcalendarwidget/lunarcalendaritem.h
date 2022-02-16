30367e48b57b85348977b9f7770eea1949386a90
hepuyao <2410474020@qq.com>
2022-02-17 01:51:15 +0800

    copyright 跟新

--- plugin-calendar/lunarcalendarwidget/lunarcalendaritem.h
+++ plugin-calendar/lunarcalendarwidget/lunarcalendaritem.h
@@ -1,4 +1,22 @@
-﻿#ifndef LUNARCALENDARITEM_H
+﻿/*
+ * Copyright (C) 2019 Tianjin KYLIN Information Technology Co., Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU  Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/&gt;.
+ *
+ */
+
+#ifndef LUNARCALENDARITEM_H
 #define LUNARCALENDARITEM_H
 
 #include <QWidget>
